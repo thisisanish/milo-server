cae0bf69ac68c668d25b0b10f286b29577e1c1ca
thisisanish <email.anishagarwal@gmail.com>
2021-03-25 13:56:43 +0530

    initial commit

--- services/doubt/model.js
+++ services/doubt/model.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-
+// Thoughts : We can have a nain Question and a supporting sub text tather then the Title and the Main  Question.
 const doubtSchema = new mongoose.Schema(
   {
     title: {
       type: String,
       required: [true, "Title is  required"],
     },
+    // equivalent to Subject for for mail. Thinking of ditching this field.
     authorID: {
       type: mongoose.Schema.Types.ObjectId,
       // ref: ''
